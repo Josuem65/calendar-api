355a30ed44c43e80410b7313a865cffc855243c5
Andre Hammons <dev@ahammons.me>
2023-07-08 23:06:39 -0700

    feat: add demo client

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 require("dotenv").config();
-const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 
@@ -8,29 +7,32 @@ const userRoutes = require("./routes/users");
 
 const PORT = process.env.PORT || 8080;
 const app = express();
-app.use(require("cors")({
-    origin: "*",
-}))
 
-// middleware parses request body as JSON
+// middleware
+app.use(
+  require("cors")({
+    origin: "*",
+  })
+);
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 // routes
 app.use("/api/events", eventRoutes);
 app.use("/api/users", userRoutes);
 
 app.get("/", (_, res) => {
-    res.sendFile(path.join(`${__dirname}/index.html`));
-})
+  res.sendFile("index.html");
+});
 
 // connect to MongoDB
 mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => {
-        app.listen(PORT, () => {
-          console.log(`Listening on port ${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.error(err);
-    })
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
